36cfee058c54d71097faec8c586bb9092ea55c05
zethra <jediben97@gmail.com>
2015-07-30 19:23:28 -0400

    Added content to about me page

--- me.php
+++ me.php
@@ -23,6 +23,46 @@
 </nav>
 
 <div class="container main">
+    <section id="who" class="text-left">
+        <p class="h3">
+            Who am I
+        </p>
+
+        <p>
+            I'm an outgoing, well-spoken nerd. I am very good with computers, I write computer programs for fun, and I
+            build computerized machines and electronics such as robots and 3-D printers.
+        </p>
+
+        <p>
+            I am very good at articulating complex concepts. Examples:
+
+        </p>
+        <ul>
+            <li>In history class this year I played Adam Smith in a discussion about whose viewpoints on government
+                systems
+                was most influential. The class later voted on which character they thought was most influential, and I
+                won
+                by a significant amount
+            </li>
+            <li>I taught HTML to my 4-H club</li>
+            <li>I gave a radio interview about how our 4-H club's water bottle dispensing robot works</li>
+            <li>Every summer at the county 4-H fair, I present my technical projects to the public</li>
+        </ul>
+    </section>
+    <section id="what" class="text-left">
+        <p class="h3">
+            What do I do
+        </p>
+
+        <p>
+            My passion is making things. I love building electronics, building software, building robots, building
+            machines. I am passionate about it. I'm always working on a project or looking for a new one, and sometimes
+            both. I am constantly researching new ways to do things and new project ideas, and looking for new
+            inspiration. I read many articles and watch many videos about new emerging technologies, current technology,
+            and various other things related to my field on a daily basis. The skills I have developed as a result of my
+            passion could easily be applied to a range of professions.
+        </p>
+    </section>
 </div>
 </body>
 </html>
