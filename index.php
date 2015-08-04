a2a0fb13a9e4dfee1f62f31aa0bc8c4ef396f7ca
zethra <jediben97@gmail.com>
2015-08-04 19:41:31 -0400

    Moved about me to index and added placeholder with names and description for the rest fo the project so far

--- index.php
+++ index.php
@@ -28,59 +28,64 @@
             Ben Goldberg
         </p>
 
-        <div id="header-text" class="container text-left">
-            <p>
-                Welcome to my web site. The purpose of this site is for you to learn about me. As I apply to colleges, I
-                will use this site as a forum for describing the key influences that have made me who I am, as well as
-                to
-                highlight the many independent and team projects I have completed, and which illustrate my initiative
-                and
-                technical abilities.
-            </p>
+        <div class="container">
+            <div class="row">
+                <div id="header-text" class="text-left col-md-8">
+                    <p>
+                        <strong>Welcome to my web site.</strong> The purpose of this site is for you to learn about me.
+                        I am
+                        using this site as a
+                        forum for describing the key influences that have made me who I am, as well as to highlight the
+                        many
+                        independent and team projects I have completed, illustrating my initiative and technical
+                        abilities.
+                    </p>
 
-            <p>The "About" section that follows summarizes the things that make me tick.</p>
-            <ul>
-                <li><p>"About Me" is obvious; I talk about who I am.</p></li>
-                <li>
-                    <p>"About Daleks" is about my high school FIRST Robotics Team, which has had a very strong influence
-                        in
-                        my
-                        process of discovering what I love to do.
+                    <p>
+                        <strong>I would call myself an outgoing, well-spoken nerd.</strong> I intend to study Computer
+                        Science,
+                        possibly
+                        concentrating in cyber security or data analytics. I am applying to schools which I believe will
+                        provide
+                        a strong computer science curriculum, but which will also provide me exposure to computer
+                        engineering,
+                        entrepreneurship, and practical applications.
                     </p>
-                </li>
-                <li><p>"About Robostorm" is about the 4-H robotics club that I started as a 7th grader, and which has
-                        had a
-                        multi-faceted influence in shaping who I am.
+
+                    <p>
+                        <strong>I am self-driven and my passion is programming and making things.</strong> I love
+                        building
+                        software, building
+                        electronics, building robots, and building machines. If I'm not working on a project, I'm
+                        looking
+                        for a
+                        new one. I am constantly researching new ways to do things, and looking for new inspiration.
+                        Every
+                        day I
+                        read articles and watch videos about new emerging technologies, current technology, and other
+                        things
+                        related to my field.
                     </p>
-                </li>
-            </ul>
-            <p>
-                Finally, the "Projects" section provides a summary of fifteen different projects of which I am very
-                proud. Most are programming and robotics/electronics projects. For each one, I describe the problem
-                being
-                solved, the technologies used and what I taught myself, and finally how it works and my process of
-                making
-                it. The last two projects are the odd ones: a business I ran after freshman year, and my
-                4-H-award-winning
-                photographs.
-            </p>
+
+                    <p>
+                        <strong>I enjoy talking to people, whether the topic is technology or a debate on current
+                            events.</strong> I have given many presentations, including a radio interview, at the annual
+                        County
+                        Fair where my 4-H robotics club exhibits its work. I am skilled at explaining technical
+                        concepts. I
+                        am
+                        also a decent salesman. Part of my pen business involved selling to the public.
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <img src="assets/me.jpg" class="img-circle">
+                </div>
+            </div>
         </div>
     </header>
     <section id="about" class="section-alt">
         <div class="container">
             <p class="section-header">About</p>
-            <a href="me.php">
-                <figure class="figure-effect">
-                    <img src="assets/me-front.jpg"/>
-                    <figcaption>
-                        <h2>About <span>Me</span></h2>
-
-                        <div>
-                            <p>Who I am and what I do</p>
-                        </div>
-                    </figcaption>
-                </figure>
-            </a>
             <a href="daleks.php">
                 <figure class="figure-effect">
                     <img src="assets/daleks-front.jpg"/>
@@ -116,7 +121,7 @@
                     <!--                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>-->
                     <img src="assets/nametag-front.png">
                     <figcaption>
-                        <h2>Name Tag <span>Auto Printing</span></h2>
+                        <h2>Name <span>Tag</span> <br/>Auto <span>Printing</span></h2>
 
                         <div>
                             <p>A web app used to order custom</p>
@@ -130,7 +135,8 @@
                 <figure class="figure-effect">
                     <img src="assets/qbert-front.jpg">
                     <figcaption>
-                        <h2>Qbert <span>The Robot</span></h2>
+                        <h2><span>Qbert</span><br/>
+                            The <span>Robot</span></h2>
 
                         <div>
                             <p>A robot with many control methods</p>
@@ -143,10 +149,171 @@
                     <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
                     <!--                    <img src="assets/nametag-front.png">-->
                     <figcaption>
-                        <h2>This is <span>A Placeholder</span></h2>
+                        <h2>Scouting <span>App</span></h2>
 
                         <div>
-                            <p>This is a placeholder</p>
+                            <p>A data collection application</p>
+
+                            <p>for FIRST competitions</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2>School <span>Navigator</span></h2>
+
+                        <div>
+                            <p>A web app that give direction</p>
+
+                            <p>around my high school</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2>3D <span>Printer</span></h2>
+
+                        <div>
+                            <p>A 3D Printer I built</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2><span>AquaBot</span></h2>
+
+                        <div>
+                            <p>A robot that drives around the</p>
+
+                            <p>4-H fair and sells water bottles</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2><span>Server</span><br/>
+                            Manager <span>App</span></h2>
+
+                        <div>
+                            <p>An android app that manages</p>
+
+                            <p>remote server applications</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2>Arcade <span>Machine</span></h2>
+
+                        <div>
+                            <p>A custom build arcade machine</p>
+
+                            <p>built for the 4-H fair</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2><span>Wi-Fi</span><br/>
+                            Light <span>Switch</span></h2>
+
+                        <div>
+                            <p>A light switch that is controlled</p>
+                            <p>with a web page</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2>Brick <span>Sorter</span></h2>
+
+                        <div>
+                            <p>A robot that sorted Lego bricks</p>
+                            <p>by color and size</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2>Robostorm <span>Club</span>
+                            <br/>Web <span>Site</span></h2>
+
+                        <div>
+                            <p>A website for the Robostorm</p>
+                            <p>Robotics 4-H club</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2>ROM <span>Bot</span></h2>
+
+                        <div>
+                            <p>A robot that took commands for a</p>
+                            <p>colored disk to solve a maze</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2>Minecraft <span>Server</span></h2>
+
+                        <div>
+                            <p>A custom Minecraft server</p>
+                        </div>
+                    </figcaption>
+                </figure>
+            </a>
+            <a href="nametags.php">
+                <figure class="figure-effect">
+                    <img src="http://www.crvdmedia.com/crvd/wp-content/uploads/2012/07/url-1.jpeg"/>
+                    <!--                    <img src="assets/nametag-front.png">-->
+                    <figcaption>
+                        <h2>Ben's <span>Pens</span></h2>
+
+                        <div>
+                            <p>A small business selling I ran</p>
+                            <p>selling hand turned wooden pens</p>
                         </div>
                     </figcaption>
                 </figure>
--- me.php
+++ me.php
@@ -25,29 +25,34 @@
     <section>
         <div class="container text-left">
             <p>
-                I would call myself an outgoing, well-spoken nerd.
+                <strong>Welcome to my web site.</strong> The purpose of this site is for you to learn about me. I am
+                using this site as a
+                forum for describing the key influences that have made me who I am, as well as to highlight the many
+                independent and team projects I have completed, illustrating my initiative and technical abilities.
             </p>
 
             <p>
-                My passion is making things. I love building electronics, building software, building robots, building
-                machines.
-                If I'm not working on a project, I'm looking for a new one. I am constantly researching new ways to do
-                things
-                and new project ideas, and looking for new inspiration. Every day I read articles and watch videos about
-                new
-                emerging technologies, current technology, and other things related to my field.
+                <strong>I would call myself an outgoing, well-spoken nerd.</strong> I intend to study Computer Science,
+                possibly
+                concentrating in cyber security or data analytics. I am applying to schools which I believe will provide
+                a strong computer science curriculum, but which will also provide me exposure to computer engineering,
+                entrepreneurship, and practical applications.
             </p>
 
             <p>
-                I enjoy talking to people, whether the topic is technology or a debate on current events. I have given
-                many
-                presentations, including a radio interview, at the annual County Fair where my 4-H robotics club
-                exhibits
-                its
-                work. I am skilled at explaining technical concepts. I am also a decent salesman. Part of my pen
-                business
-                involved selling to the public.
+                <strong>I am self-driven and my passion is programming and making things.</strong> I love building
+                software, building
+                electronics, building robots, and building machines. If I'm not working on a project, I'm looking for a
+                new one. I am constantly researching new ways to do things, and looking for new inspiration. Every day I
+                read articles and watch videos about new emerging technologies, current technology, and other things
+                related to my field.
+            </p>
 
+            <p>
+                <strong>I enjoy talking to people, whether the topic is technology or a debate on current
+                    events.</strong> I have given many presentations, including a radio interview, at the annual County
+                Fair where my 4-H robotics club exhibits its work. I am skilled at explaining technical concepts. I am
+                also a decent salesman. Part of my pen business involved selling to the public.
             </p>
         </div>
     </section>
