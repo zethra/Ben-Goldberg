4145291b6fe38160f5e11164a58d6445e883fcd4
zethra <jediben97@gmail.com>
2015-08-06 17:03:09 -0400

    Added content to about daleks

--- daleks.php
+++ daleks.php
@@ -12,17 +12,63 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand page-scroll" href="#page-top">Ben Goldberg</a>
+            <a class="navbar-brand page-scroll" href="index.php">Ben Goldberg</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="index.php">Back</a></li>
             </ul>
         </div>
     </div>
 </nav>
 
 <div class="container main">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="container text-left">
+                <p>
+                    I joined my high school FIRST robotics team as a Sophomore, and continued with the team through
+                    Junior and Senior years. Team 3637, the Daleks, is now entering its sixth year competing and each
+                    year we are more successful. Being a Dalek has been a very meaningful and focusing experience for
+                    me.
+                </p>
+
+                <p>
+                    As a Junior I put my energy into solving a problem that the team had not yet addressed. When teams
+                    are competing, there is a need to "scout" out the strengths and weaknesses of the other teams'
+                    robots and use that information to help our robot drivers determine their game play strategy for
+                    each match. I made it my mission to develop a Scouting application that would bring our team's
+                    success to a new level.
+                </p>
+
+                <p>
+                    I led this initiative on my own, resulting in our team having its first automated scouting
+                    application for collecting and analyzing team performance.
+                </p>
+                <ul>
+                    <li><p>I worked with teammates on initial design decisions, and on what data to collect and analyze,
+                            based on game rules and scoring.</p></li>
+                    <li><p>I helped three teammates prototype the web GUI who were learning HTML.</p></li>
+                    <li><p>I designed and coded the entire application.</p></li>
+                    <li><p>Twenty of my teammates were trained on how to use the application.</p></li>
+                    <li><p>I designed the operation and the IT configuration used during the tournaments.</p></li>
+                </ul>
+                <p>
+                    This is my most substantial and proud accomplishment. In the "Projects" section of this site, please
+                    read about it.
+                </p>
+
+                <p>
+                    As a Senior, I have been appointed to the position of Director of Scouting and Scouting Software on
+                    the team's executive board, reporting to the Tech VP. My goal this year is to enhance the Scouting
+                    application so that it can be more easily configured to apply to each year's new game. A new FIRST
+                    game will be announced in January, which the robot will be built to play. This means a new game play
+                    strategy and new data collection and analysis will be required. With the improvements I plan to
+                    make, others will be able to easily adapt the application each year, and the team will be able to
+                    benefit from its analytics.
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
