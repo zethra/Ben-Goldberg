7f1ce9e4dceb6d04b328aec59c0cd81a552030f4
zethra <jediben97@gmail.com>
2015-08-25 17:26:16 -0400

    Added Robostorm content

--- robostorm.php
+++ robostorm.php
@@ -22,6 +22,58 @@
 </nav>
 
 <div class="container main">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="container text-left">
+                <p>
+                    I started Hunterdon County's first 4-H robotics club, and I was the club's first President.
+                </p>
+
+                <p>
+                    As a seventh grader, after extreme disappointment over my school's Lego robotics club folding, I
+                    decided to take matters into my own hands and start my own club. I learned about 4-H, but that
+                    Hunterdon County's 4-H did not have a robotics club. So I started one, in the spring of 2011. We
+                    started with six members and grew to twenty over two years. I was so happy to meet other kids with
+                    similar interests! We were soon turning people away. We have had thirty-one different members over
+                    our 4-1/2 years.
+                </p>
+
+                <p>
+                    I was elected President for our first full year, when I was in eighth grade. I ran our meetings and
+                    helped the club decide on projects. For our first year's community service project, we all brought
+                    our robots to a local assisted living home, and we presented them to the residents. They loved us!
+                    As the club grew, I mentored some of the younger members. As Corresponding Secretary for the club's
+                    second year, I learned HTML in high school and decided to build a club web site. I also taught an
+                    HTML class to my clubmates.
+                </p>
+
+                <p>
+                    By this time, Robostorm had taken on a life of its own. We had more parents who agreed to become
+                    leaders, and who brought their technical experience to the club. After our first two years focusing
+                    on the Lego NXT platform, we were being introduced to the Arduino microcontroller, the Raspberry Pi
+                    computer, mechanical design, programming, and wiring and soldering. These influences put me on a new
+                    trajectory as a sophomore. The substance, sophistication, and complexity of the projects I took on
+                    were at a new level. In the "Projects" section of this site, please read about my independent
+                    projects as well as the club group projects I worked on.
+                </p>
+
+                <p>
+                    Every year since I started Robostorm, I have spent a portion of my summer preparing for and
+                    participating in the 5-day exhibition at the Hunterdon County 4-H Fair. During the fair my club and
+                    I demonstrate our various robotics and technology projects to the community, which we have prepared
+                    in the months prior. Some of my best projects were prepared for the 4-H fair.
+                </p>
+
+                <p>
+                    Looking back I am amazed at what Robostorm has become, and that it would not have existed, had I
+                    just walked away from the news of no robotics in seventh grade. I feel that the influence and impact
+                    of this experience on all of the club members and families has been significant. The club still
+                    thrives. It has provided a place for technology-minded children across the county to find friends
+                    and to experiment and explore interesting technology-oriented projects.
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 <?php require 'jsIncludes.php'; ?>
 </body>
