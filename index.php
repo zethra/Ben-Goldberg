3d40116c832afbed6c45f8ef521afc2b54f3116e
zethra <jediben97@gmail.com>
2015-09-19 19:14:59 -0400

    Centered image

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     </div>
 </nav>
 <div class="container-fluid main">
-    <header>
+    <header id="mainHeader">
         <p class="page-header-custom">
             Ben Goldberg
         </p>
@@ -35,48 +35,35 @@
                     <p>
                         <strong>Welcome to my web site.</strong> The purpose of this site is for you to learn about me.
                         I am using this site as a forum for describing the key influences that have made me who I am, as
-                        well as to highlight the many independent and team projects I have completed, illustrating my
-                        initiative and technical abilities.
+                        well as to highlight the many independent and team projects I have completed that
+                        illustrate my initiative and technical ability.
                     </p>
 
                     <p>
-                        <strong>I would call myself an outgoing, well-spoken nerd.</strong> I intend to study Computer
-                        Science,
-                        possibly
-                        concentrating in cyber security or data analytics. I am applying to schools which I believe will
-                        provide
-                        a strong computer science curriculum, but which will also provide me exposure to computer
-                        engineering,
-                        entrepreneurship, and practical applications.
+                        <strong>I would call myself an outgoing, well-spoken geek.</strong> I intend to study Computer
+                        Science, possibly concentrating in cyber security or data analytics. I am applying to schools
+                        which I believe will provide a strong computer science curriculum, but which will also
+                        provide me exposure to computer engineering, entrepreneurship, and practical applications.
                     </p>
 
                     <p>
                         <strong>I am self-driven and my passion is programming and making things.</strong> I love
-                        building
-                        software, building
-                        electronics, building robots, and building machines. If I'm not working on a project, I'm
-                        looking
-                        for a
-                        new one. I am constantly researching new ways to do things, and looking for new inspiration.
-                        Every
-                        day I
-                        read articles and watch videos about new emerging technologies, current technology, and other
-                        things
-                        related to my field.
+                        building software, electronics, robots, and machines. If I'm not working on a project, I'm
+                        looking for a new one. I am constantly researching new ways to do things, and looking for new
+                        inspiration. Every day I read articles and watch videos about new emerging technologies,
+                        current technology, and other things related to my field.
                     </p>
 
                     <p>
                         <strong>I enjoy talking to people, whether the topic is technology or a debate on current
                             events.</strong> I have given many presentations, including a radio interview, at the annual
-                        County
-                        Fair where my 4-H robotics club exhibits its work. I am skilled at explaining technical
-                        concepts. I
-                        am
-                        also a decent salesman. Part of my pen business involved selling to the public.
+                        County Fair where my 4-H robotics club exhibits its work. I am skilled at explaining
+                        technical concepts. I am also a decent salesman. Part of my pen business involved selling to
+                        the public.
                     </p>
                 </div>
-                <div class="col-md-4">
-                    <img src="assets/me.jpg" class="img-circle img-responsive" height="400" width="400">
+                <div class="col-md-4 text-center">
+                    <img src="assets/me.jpg" class="img-center img-circle img-responsive" height="400" width="400">
                 </div>
             </div>
         </div>
